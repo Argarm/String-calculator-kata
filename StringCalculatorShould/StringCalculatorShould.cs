7b28331c8ee41b7bd56713cf3d154c9440703cb7
Aarón García Marrero (AIDA) <aargarcia@gda.dis>
2019-10-04 17:08:10 +0100

    primerTestRojo

---
+++ StringCalculatorShould/StringCalculatorShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorShould
+{
+    [TestClass]
+    public class StringCalculatorShould
+    {
+        StringCalculator pruebas = new StringCalculator();
+        [TestMethod]
+        public void Return_0_When_Input_Is_Empty_String()
+        {
+            Assert.AreEqual(0, pruebas.add(""));
+        }
+    }
+}
