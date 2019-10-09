a6af1578ac244ae0d6d546bc62ab44546d68c73b
Aarón García Marrero (AIDA) <aargarcia@gda.dis>
2019-10-09 10:33:03 +0100

    red return 3 when input have LF

--- StringCalculatorShould/StringCalculatorShould.cs
+++ StringCalculatorShould/StringCalculatorShould.cs
@@ -8,34 +8,40 @@ namespace StringCalculatorShould
     {
         StringCalculator pruebas = new StringCalculator();
         [TestMethod]
-        public void Return_0_When_Input_Is_Empty_String()
+        public void return_0_when_input_is_empty_string()
         {
             Assert.AreEqual(0, pruebas.add(""));
         }
 
         [TestMethod]
-        public void Return_Number_When_Input_Is_That_Number()
+        public void return_number_when_input_is_that_number()
         {
             Assert.AreEqual(2, pruebas.add("2"));
         }
 
         [TestMethod]
-        public void Return_3_When_Input_Is_1_And_2()
+        public void return_3_when_input_is_1_and_2()
         {
             Assert.AreEqual(3, pruebas.add("1,2"));
         }
 
         [TestMethod]
-        public void Return_Sum_When_Input_Are_2_numbers()
+        public void return_sum_when_input_are_2_numbers()
         {
             Assert.AreEqual(7, pruebas.add("5,2"));
         }
 
         [TestMethod]
-        public void Return_6_When_Input_Are_1_2_3()
+        public void return_6_when_input_are_1_2_3()
         {
             Assert.AreEqual(6, pruebas.add("1,2,3"));
         }
+        [TestMethod]
+        public void return_3_when_input_have_LF()
+        {
+            Assert.AreEqual(3, pruebas.add("1\n2"));
+        }
+        
 
     }
 }
