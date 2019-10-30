c8cbd70e5a116f412ddba9fd5fc001b36a921a1e
argarm <aarongrmrr@gmail.com>
2019-10-30 16:22:34 +0000

    adding swagger

--- kata_String_Calculator/StringCalculatorAPI/Startup.cs
+++ kata_String_Calculator/StringCalculatorAPI/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace StringCalculatorAPI
 {
@@ -18,27 +20,51 @@ namespace StringCalculatorAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "String Calculator API",
+                    Description = "API que suma una serie de numeros pasados",
+                    TermsOfService = "#PuxaSporting",
+                    License = new License
+                    {
+                        Name = "MIT",
+                        Url = "https://es.wikipedia.org/wiki/Licencia_MIT"
+                    },
+                    Contact = new Contact
+                    {
+                        Name = "Aarón García",
+                        Email = "aargarcia@domingoalonsogroup.com",
+                        Url = "github.com/Argarm"
+                    }
+                });
+            });
+
+
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapControllers();
-            });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SPORTING API V1"); c.RoutePrefix=String.Empty;});
+            
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
